edc16b13d825ef7102150d16525869d7efed1290
kjone8371 <kjone8371@gmail.com>
2024-06-18 11:08:20 +0900

    오류 발견

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
+import New from './pages/New';
+import Read from './pages/Read';
+import { BookProvider } from './context/BookContext';
+
+function App() {
+    return (
+        <BookProvider>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/new" element={<New />} />
+                    <Route path="/read/:id" element={<Read />} />
+                </Routes>
+        </BookProvider>
+    );
+}
+
+export default App;
